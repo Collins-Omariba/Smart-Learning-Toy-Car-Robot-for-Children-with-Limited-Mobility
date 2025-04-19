19d859a11119796df832c879b0f972ece4268d01
Collins Omariba <collinsomariba@gmail.com>
2025-04-19 14:05:40 +0300

    Add color constants

--- wyoming-satellite/wyoming_satellite/satellite.py
+++ wyoming-satellite/wyoming_satellite/satellite.py
@@ -1244,6 +1244,13 @@ GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
+CYAN    = (0, 255, 255)
+MAGENTA = (255, 0, 255)
+WHITE   = (255, 255, 255)
+ORANGE  = (255, 128, 0)
+PURPLE  = (128, 0, 128)
+PINK    = (255, 192, 203)
+LIME    = (128, 255, 0)
 
 # APA102 Class (from your LED script)
 class APA102:
